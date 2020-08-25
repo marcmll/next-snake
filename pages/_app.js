0a504845702776d6ef2b2e3a71fc99577d70d36b
Marc Mueller <momueller@me.com>
2020-08-25 23:24:12 +0200

    Updated _app.js
    Changes Include:
    • Now importing FontAwesome styles to prevent flicker on load
    • Using the FontAwesome config to turn off CSS auto-insert

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
+// Import SCSS
 import 'styles/main.scss'
-import { library } from '@fortawesome/fontawesome-svg-core'
+// Import initial FontAwesome Styles: https://github.com/FortAwesome/react-fontawesome/issues/134#issuecomment-476276516
+import '@fortawesome/fontawesome-svg-core/styles.css'
+
+// Import FontAwesome Icons
+import { config, library } from '@fortawesome/fontawesome-svg-core'
 import { faStar, faArrowUp, faArrowRight, faArrowDown, faArrowLeft, faTrophy } from '@fortawesome/free-solid-svg-icons'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 library.add(faStar, faArrowUp, faArrowRight, faArrowDown, faArrowLeft, faTrophy, faGithub)
+config.autoAddCss = false
 
 export default function MyApp ({ Component, pageProps }) {
   return <Component {...pageProps} />
